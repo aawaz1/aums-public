02c8308a0463a31d06b523091714ab208d9e720e
My Name <myemail@example.com>
2024-04-17 19:01:42 +0530

    first

--- src/components/AdmissionForm.js
+++ src/components/AdmissionForm.js
@@ -1,12 +1,8 @@
 import { Box, Grid } from '@mui/material'
 import React from 'react'
 import './AdmissionForm.css';
-import Accordion from '@mui/material/Accordion';
-import AccordionActions from '@mui/material/AccordionActions';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Button from '@mui/material/Button';
+
+import Carousal from './carousal';
 
 const AdmissionForm = () => {
     return (
@@ -15,71 +11,10 @@ const AdmissionForm = () => {
                 <h4 className='form'>Personal Information</h4>
             </Box>
             <Box>
-                <div>
-                    <Accordion sx={{ padding: "1rem" }}>
-                        <AccordionSummary
-                            expandIcon={<ExpandMoreIcon />}
-                            aria-controls="panel1-content"
-                            id="panel1-header"
-                            sx={{ color: "black", backgroundColor: "lightgray", borderRadius: '10px', fontSize: "1.2rem", }}
-                        >
-                            Personal Details
-                        </AccordionSummary>
-                        <AccordionDetails sx={{ paddingTop: '1rem', paddingBottom: "1rem" }}>
-                            <Grid container spacing={1}>
-                                <Grid item container spacing={1}>
-                                    <Grid xs={12} md={4} item>
-                                        <div style={{ display: "flex", justifyContent: "flex-start", flexDirection: "column" }}>
-                                            <label className='label'>First Name</label>
-                                            <input style={{ backgroundColor: "lightgray", border: "none", borderRadius: '8px', height: "2rem" }} />
-                                        </div>
-                                    </Grid>
-                                    <Grid xs={12} md={4} item> <div style={{ display: "flex", justifyContent: "flex-start", flexDirection: "column" }}>
-                                        <label className='label' >Middle Name</label>
-                                        <input style={{ backgroundColor: "lightgray", border: "none", borderRadius: '8px', height: "2rem" }} />
-                                    </div></Grid>
-                                    <Grid xs={12} md={4} item > <div style={{ display: "flex", justifyContent: "flex-start", flexDirection: "column" }}>
-                                        <label className='label'>Family Name</label>
-                                        <input style={{ backgroundColor: "lightgray", border: "none", borderRadius: '8px', height: "2rem" }} />
-                                    </div></Grid>
-
-
-                                </Grid>
-                                <Grid item container spacing={1}>
-                                    <Grid xs={12} md={4} item>
-                                        <div style={{ display: "flex", justifyContent: "flex-start", flexDirection: "column" }}>
-                                            <label className="label">DOB</label>
-                                            <input style={{ backgroundColor: "lightgray", border: "none", borderRadius: '8px', height: "2rem" }} />
-                                        </div>
-                                    </Grid>
-                                    <Grid xs={12} md={4} item> <div style={{ display: "flex", justifyContent: "flex-start", flexDirection: "column" }}>
-                                        <label className="label">Gender</label>
-                                        <input style={{ backgroundColor: "lightgray", border: "none", borderRadius: '8px', height: "2rem" }} />
-                                    </div></Grid>
-                                    <Grid xs={12} md={4} item > <div style={{ display: "flex", justifyContent: "flex-start", flexDirection: "column" }}>
-                                        <label className="label">Martial Status</label>
-                                        <input style={{ backgroundColor: "lightgray", border: "none", borderRadius: '8px', height: "2rem" }} />
-                                    </div></Grid>
-
-
-                                </Grid>
-                                <Grid item container spacing={1}>
-                                    <Grid xs={12} md={4} item>
-                                        <div style={{ display: "flex", justifyContent: "flex-start", flexDirection: "column" }}>
-                                            <label className="label">Civil ID Number</label>
-                                            <input style={{ backgroundColor: "lightgray", border: "none", borderRadius: '8px', height: "2rem" }} />
-                                        </div>
-                                    </Grid>
-
-
-
-                                </Grid>
-                            </Grid>
-                        </AccordionDetails>
-                    </Accordion>
-
-
-                </div>
+                <Carousal title={"Personal Details"} />
+                <Carousal title={"Address Details"} />
+                <Carousal title={"Contact Details"} />
+                <Carousal title={"Nationality & Residency"} />
             </Box>
         </Box>
     )
---
+++ src/components/data.js
@@ -0,0 +1,38 @@
+export const Data = {
+    Data1 :  [ {
+        id :1, 
+        screen : 4,
+        label : "First Name",
+
+    },
+    {
+        id :2, 
+        screen : 4,
+        label : "Middle Name",
+
+    },
+    {
+        id :3, 
+        screen : 4,
+        label : "Family Name",
+
+    },
+] , 
+Data2 :  [ {
+    id :1, 
+    label : "Date Of Birth",
+
+},
+{
+    id :2, 
+    label : "Gender",
+
+},
+{
+    id :3, 
+    label : "Martial Status",
+
+},
+] , 
+
+}
